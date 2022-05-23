3ede6c0d3a40f30f06532b373973827a127f96a8
Felipe Bedê <felipebede@gmail.com>
2022-05-23 02:44:39 -0300

    inclusao do swagger

--- src/main/java/br/com/flpbd/email/controller/EmailController.java
+++ src/main/java/br/com/flpbd/email/controller/EmailController.java
@@ -3,16 +3,17 @@ package br.com.flpbd.email.controller;
 import br.com.flpbd.email.model.EmailModel;
 import br.com.flpbd.email.model.dto.EmailDTO;
 import br.com.flpbd.email.services.EmailService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/send-email")
 public class EmailController {
     final EmailService emailService;
 
@@ -20,7 +21,8 @@ public class EmailController {
         this.emailService = emailService;
     }
 
-    @PostMapping("/sending-email")
+    @PostMapping
+    @ApiOperation("Tste Swagger")
     public ResponseEntity<EmailModel> sendingEmail(@RequestBody @Valid EmailDTO emailDTO) {
         EmailModel emailModel = new EmailModel();
         BeanUtils.copyProperties(emailDTO, emailModel);
